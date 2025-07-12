79fbb25d5cd21557d04dd96310931e479857c8a0
tjgtll <urkona@list.ru>
2025-07-12 23:09:10 +0300

    fix: add JsonPropertyOrder

--- Energomera.DTO/FieldBasicDto.cs
+++ Energomera.DTO/FieldBasicDto.cs
@@ -1,7 +1,12 @@
-﻿namespace Energomera.DTO;
+﻿using System.Text.Json.Serialization;
+
+namespace Energomera.DTO;
 
 public class FieldBasicDto
 {
+    [JsonPropertyOrder(1)]
     public int Id { get; set; }
+
+    [JsonPropertyOrder(2)]
     public string Name { get; set; }
 }
--- Energomera.DTO/FieldDto.cs
+++ Energomera.DTO/FieldDto.cs
@@ -1,7 +1,12 @@
-﻿namespace Energomera.DTO;
+﻿using System.Text.Json.Serialization;
+
+namespace Energomera.DTO;
 
 public class FieldDto : FieldBasicDto
 {
+    [JsonPropertyOrder(3)]
     public double Size { get; set; }
+    
+    [JsonPropertyOrder(4)]
     public LocationsDto Locations { get; set; }
 }
